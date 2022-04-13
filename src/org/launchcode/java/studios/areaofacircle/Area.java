3fdbb8058476163fc6ecf38e335d679717b6c165
unknown <mddscudder44@gmail.com>
2022-04-13 13:55:06 -0400

    just moved some quotes

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle:");
+        System.out.println("Enter the radius of the circle: ");
         double radius = input.nextDouble();
         double area = 3.14 * radius *radius;
-        System.out.println(" The area of the circle is :"+ area);
+        System.out.println("The area of the circle is: "+ area);
 
 
     }
