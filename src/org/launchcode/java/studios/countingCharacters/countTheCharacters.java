7b541347aa10d6bf8c8865ed520efbfeaeba1e6d
Raiden DeKoter <raiden.dekoter@gmail.com>
2022-04-14 19:38:22 -0500

    finished studio

--- src/org/launchcode/java/studios/countingCharacters/countTheCharacters.java
+++ src/org/launchcode/java/studios/countingCharacters/countTheCharacters.java
@@ -1,16 +1,24 @@
 package org.launchcode.java.studios.countingCharacters;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class countTheCharacters {
-    public static void main(String[] args) {
-        String phrase = "Sample Sentence";
-        char[] characters = phrase.toCharArray();
+  public static void main(String[] args) {
+    String phrase = "Sample Sentence";
+    char[] characters = phrase.toCharArray();
+    HashMap<Character, Integer> charCounts = new HashMap<>();
 
-        HashMap<Character, Integer> charCounts = new HashMap<>();
-        for (int i = 0; i < characters.length; i++) {
-            char theCharacter = characters[i];
-            //System.out.println(theCharacter);
-        }
+    for (int i = 0; i < characters.length; i++) {
+      if (charCounts.containsKey(characters[i])) {
+        charCounts.put(characters[i], charCounts.get(characters[i]) + 1);
+      } else {
+        charCounts.put(characters[i], 1);
+      }
     }
+
+    for (Map.Entry<Character, Integer> charCount : charCounts.entrySet()) {
+      System.out.println(charCount.getKey() + ": " + charCount.getValue());
+    }
+  }
 }
