4c201ff8c8097fe1406467b4c40d77e9c0364892
salev <salevillain@gmail.com>
2022-04-18 20:20:16 -0500

    implemented menuItem

--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,54 @@
 package restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+    private Date dateAdded;
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public MenuItem(String name, String description, String category, double price, Date dateAdded) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.dateAdded = dateAdded;
+    }
+
+    public boolean isNew(int newThreshold) {
+        return new Date().compareTo(dateAdded) > newThreshold;
+    }
 }
