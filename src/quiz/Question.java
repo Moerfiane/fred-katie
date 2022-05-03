5999d521e718cd90d40475ee66208b3480f600eb
Maddie <mddscudder44@gmail.com>
2022-05-02 20:24:18 -0400

    Created Question Class

--- src/quiz/Question.java
+++ src/quiz/Question.java
@@ -1,4 +1,50 @@
 package quiz;
 
-public class Question {
+import java.util.Objects;
+
+public abstract class Question {
+    private String prompt;
+    private String answer;
+    private Double points;
+
+    public Question(String prompt, String answer, Double points) {
+        this.prompt = prompt;
+        this.answer = answer;
+        this.points = points;
+    }
+
+    public Question(String prompt, String answer){
+        this(prompt, answer, 1.0);
+    }
+
+    public abstract Double grade();
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public Double getPoints() {
+        return points;
+    }
+
+    public void setPoints(Double points) {
+        this.points = points;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question)) return false;
+        Question question = (Question) o;
+        return prompt.equals(question.prompt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prompt);
+    }
 }
